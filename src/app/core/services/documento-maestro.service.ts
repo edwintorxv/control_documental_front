a549899802c1965110edf218727216d5d4167ba1
Edwin Gacha <edwintorxv@yahoo.es>
2026-02-22 18:22:38 -0500

    Creacion inicial de servicios' clear

---
+++ src/app/core/services/documento-maestro.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { DocumentoMaestro } from '../../shared/models/documento-maestro.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class DocumentoMaestroService{
+    
+}
