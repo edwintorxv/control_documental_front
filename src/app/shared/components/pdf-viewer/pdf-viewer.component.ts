eeb4e4279f3c72efae089bbd3c7f33bde83160a2
EDWIN GUSTAVO GACHA AMADO <gachae@HILDEBOG.LOCAL>
2026-03-05 17:55:55 -0500

    Finalizacon crud documentos y visor de pdf

--- src/app/shared/components/pdf-viewer/pdf-viewer.component.ts
+++ src/app/shared/components/pdf-viewer/pdf-viewer.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-pdf-viewer',
   standalone: true,
   imports: [
     CommonModule,
-    PdfViewerModule
+    PdfViewerModule,
+    ButtonModule
   ],
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss']
@@ -15,6 +17,40 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
 export class PdfViewerComponent {
 
   @Input() src!: string;
-  @Input() height: string = '600px';
+  @Input() height: string = '80vh';
+
+  zoom: number = 1;
+  rotation: number = 0;
+
+  showPdf = true;
+
+  refreshViewer() {
+    this.showPdf = false;
+
+    setTimeout(() => {
+      this.showPdf = true;
+    }, 50);
+  }
+
+  zoomIn() {
+    this.zoom += 0.25;
+    this.refreshViewer();
+  }
+
+  zoomOut() {
+    if (this.zoom > 0.5) {
+      this.zoom -= 0.25;
+      this.refreshViewer();
+    }
+  }
+
+  rotate() {
+    this.rotation = (this.rotation + 90) % 360;
+    this.refreshViewer();
+  }
+
+  downloadPdf() {
+    window.open(this.src, '_blank');
+  }
 
 }
